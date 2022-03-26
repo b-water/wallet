8c96cf293393638ede0d355b0219f0349a7cba83
Nico <bwaterxo@gmail.com>
2022-03-26 09:50:07 +0100

    fix api routes

--- api/index.js
+++ api/index.js
@@ -1,42 +1,14 @@
 import express from 'express'
-import { PrismaClient } from '@prisma/client'
 import currency from './routes/currency'
+import transaction from './routes/transaction'
 
-const prisma = new PrismaClient()
-const router = express()
+const app = express()
 
 export default {
     path: '/api',
-    handler: router
+    handler: app
 }
 
-router.use(express.json())
-router.use(currency)
-
-router.post(`/transaction`, async (req, res) => {
-    const result = await prisma.transaction.create({
-        data: {
-            type: req.body.type,
-            name: req.body.name,
-            value: req.body.value
-        },
-    })
-    res.json(result)
-})
-
-router.post('/transaction', async (req, res) => {
-    const { type, name, value } = req.body
-    const transaction = await prisma.transaction.create({
-        data: {
-            type,
-            name,
-            value
-        }
-    })
-    res.status(200).json(transaction)
-})
-
-router.get('/transactions', async (req, res) => {
-    const result = await prisma.transaction.findFirst()
-    res.json(result)
-})
+app.use(express.json())
+app.use(currency)
+app.use(transaction)
--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -3,3 +3,33 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const router = Router()
+
+router.post(`/transaction`, async (req, res) => {
+    const result = await prisma.transaction.create({
+        data: {
+            type: req.body.type,
+            name: req.body.name,
+            value: req.body.value
+        },
+    })
+    res.json(result)
+})
+
+router.post('/transaction', async (req, res) => {
+    const { type, name, value } = req.body
+    const transaction = await prisma.transaction.create({
+        data: {
+            type,
+            name,
+            value
+        }
+    })
+    res.status(200).json(transaction)
+})
+
+router.get('/transactions', async (req, res) => {
+    const result = await prisma.transaction.findFirst()
+    res.json(result)
+})
+
+export default router;
